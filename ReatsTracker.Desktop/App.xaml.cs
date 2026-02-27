d51d7eba9a70e3c9cfbabab6456269e12c845fb4
vzalxndr <vzalxndr.reserve@gmail.com>
2026-02-27 09:25:43 +0100

    init: create initial WPF project

---
+++ ReatsTracker.Desktop/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ReatsTracker.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
