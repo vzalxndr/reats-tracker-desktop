e9c8efb60b3ce351b4394e19e340d05dffa0547f
vzalxndr <vzalxndr.reserve@gmail.com>
2026-03-05 16:18:19 +0100

    style: change delete button color

--- ReatsTracker.Desktop/ViewModels/MainViewModel.cs
+++ ReatsTracker.Desktop/ViewModels/MainViewModel.cs
@@ -66,11 +66,14 @@ namespace ReatsTracker.Desktop.ViewModels
 
         public async Task DeleteCompanyAsync()
         {
-            if (SelectedCompany == null) return;
+            if (SelectedCompany == null)
+            {
+                return;
+            }
 
             var response = await _apiService.DeleteCompanyAsync(SelectedCompany.Id);
+
             if (response)
-            {
             {
                 Companies.Remove(SelectedCompany);
             }
